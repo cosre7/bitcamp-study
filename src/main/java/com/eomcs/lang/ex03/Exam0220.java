72efe7474d29daafb4a71fd15987f01fa7e19052
cosre7 <cosre7@gmail.com>
2021-01-09 20:38:11 +0900

    .

--- src/main/java/com/eomcs/lang/ex03/Exam0220.java
+++ src/main/java/com/eomcs/lang/ex03/Exam0220.java
@@ -5,16 +5,16 @@ package com.eomcs.lang.ex03;
 //
 public class Exam0220 {
   public static void main(String[] args) {
-    
+
     //## 10진수에 _ 문자를 삽입하기
     System.out.println(12783406);
-    System.out.println(1278_3406);
-    System.out.println(12_783_406);
+    System.out.println(1278_3406); // 일상에서 사용하는 1278,3406 같은 느낌
+    System.out.println(12_783_406); // 일상에서 사용하는 12,783,406 같은 느낌
 
     //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
     //System.out.println(_12783406); 
     //System.out.println(12783406_);
-    
+
     //## 8진수에 _ 문자를 삽입하기
     System.out.println(077);
     System.out.println(0_77);
