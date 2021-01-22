e62751eab89a746ccf82f80db2fea9470eddee93
cosre7 <cosre7@gmail.com>
2021-01-22 11:43:07 +0900

    .

--- src/main/java/test/ex01/App.java
+++ src/main/java/test/ex01/App.java
@@ -3,5 +3,11 @@ package test.ex01;
 public class App {
   public static void main(String[] args) {
 
+    // 스태틱 필드 명령에 따라 변수를 생성하기
+    // => 클래스를 로딩해야 그 클래스의 스태틱 필드가 생성된다.
+    // 
+    // 언제 클래스를 로딩하는가?
+    // => 클래스를 사용할 때 JVM이 자동으로 로딩한다.
+
   }
 }
