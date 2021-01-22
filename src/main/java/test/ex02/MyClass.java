38f63c8c4b61aaed611a26f223f61bd3931a0dd1
cosre7 <cosre7@gmail.com>
2021-01-22 13:42:43 +0900

    .

--- src/main/java/test/ex02/MyClass.java
+++ src/main/java/test/ex02/MyClass.java
@@ -3,7 +3,10 @@ package test.ex02;
 public class MyClass {
 
   static int a = 100;
+  // static 필드 = class 필드 -> 클래스에서 바로 접근이 가능하다고 해서 클래스 필드라고도 함.
+  // class 가 로딩되면 바로 생성
 
   int b = 200;
+  // new 가 되어야 생성
 
 }
