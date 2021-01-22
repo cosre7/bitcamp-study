53cc0096853313586c17391eae335514524a81ec
cosre7 <cosre7@gmail.com>
2021-01-22 12:11:58 +0900

    .

--- src/main/java/test/ex02/MyClass.java
+++ src/main/java/test/ex02/MyClass.java
@@ -1,16 +1,9 @@
 package test.ex02;
 
 public class MyClass {
-  // 스태틱 필드(변수) = 클래스 필드(변수)
-  static int a = 1;
 
-  // 인스턴스 필드(변수)
-  int b = 10;
+  static int a = 100;
 
-  public static void main(String[] args) {
-    // 로컬 변수
-    int c;
-    c = 100;
-    System.out.println(c);
-  }
+  int b = 200;
+
 }
