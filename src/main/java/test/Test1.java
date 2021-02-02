1a8d48a5b5614df7e4341c33c2799fea27b25281
cosre7 <cosre7@gmail.com>
2021-02-02 12:57:15 +0900

    .

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -1,23 +1,15 @@
 package test;
 
-import test.util.Calculator;
 import test.util2.Calculator2;
 
 public class Test1 {
   public static void main(String[] args) {
     Calculator2 c = new Calculator2(); 
-    //    c.plus(100);
-    //    c.minus(20); 
-    //    System.out.println(c.getResult());
-
-    Calculator x = c;
-    // 상위 분류는 하위 분류를 가리킬 수 있다.
-    x.plus(200);
-    //x.minus(300); // 범위는 Calculator의 범위로 한정된다.
-    // minus는 Calculator2의 것이기 때문에 사용 불가.
-    ((Calculator2) x).minus(300); // 형변환으로 사용 가능
-    // x = Calculator2를 가리킬 때만 형변환으로 사용!
-    // 컴파일러를 속이지 말자!!
-    System.out.println(x.getResult());
+    c.plus(100);
+    c.plus(100, 200);
+    c.minus(20); 
+    c.minus(30, 40);
+    //    c.plus(3.14f); //float 타입은 정의하지 않았으므로 에러!
+    System.out.println(c.getResult());
   }
 }
--- src/main/java/test/util2/Calculator2.java
+++ src/main/java/test/util2/Calculator2.java
@@ -3,7 +3,22 @@ package test.util2;
 import test.util.Calculator;
 
 public class Calculator2 extends Calculator {
+
   public void minus(int value) {
     this.result -= value;
   }
+
+  public void minus(int value1, int value2) {
+    this.result -= value1;
+    this.result -= value2;
+  }
+  // minus가 있는데 또 minus 추가 -> 오버로드
+
+  public void plus(int a, int b) {
+    this.result += a;
+    this.result += b;
+    // 상속받은 슈퍼클래스에 있는 메서드를 또 추가 -> 오버로드
+  }
+  // 같은 이름을 가진 메서드가 현재 클래스나 슈퍼클래스에 존재
+  // -> 오버로드
 }
