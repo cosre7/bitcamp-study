a2ddd68dd79a6eb12d4e5ca0d3cf753afa83fe90
cosre7 <cosre7@gmail.com>
2021-02-04 11:53:56 +0900

    .

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -13,6 +13,7 @@ public class Test {
 
   static void print(Object obj) {
     System.out.println("===> " + obj.toString());
+    // string과 date -> 다형적 변수 이용
   }
 
   public static void main(String[] args) {
