15d7fa9ed37dc3d0c88c41e2d5925afeb18bdcb2
cosre7 <cosre7@gmail.com>
2021-02-04 12:03:00 +0900

    .

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,24 +1,18 @@
 package test;
 
-import java.sql.Date;
-
 public class Test {
 
+  static class Node {
+    Object value;
+    Node first;
+    Node last;
+  }
 
   public static void main(String[] args) {
-    int[] arr1 = new int[5];
-    float[] arr2 = new float[5];
-
-    arr1[0] = 100;
-    // arr1[2] = 3.14f; -> 불가능
-
-    Object[] arr3 = new Object[5];
-
-    arr3[0] = new String("hello");
-    arr3[1] = new Date(System.currentTimeMillis());
-    arr3[2] = new StringBuffer("Hello");
-    arr3[3] = Integer.valueOf(100);
-    arr3[4] = Float.valueOf(3.14f);
-    // 프리머티브 타입을 객체로 다룰 수 있다 -> wrapper 클래스의 장점
+    Node node = new Node();
+    node.value = new Integer(100);
+    node.value = Integer.valueOf(100);
+    // -> wrapper 클래스를 사용하는 이유
+    // 코드의 재활용이 가능하다
   }
 }
