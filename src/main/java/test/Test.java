4b729bb392ef91238b83ecc6d024633fcf8b74ad
cosre7 <cosre7@gmail.com>
2021-02-04 11:53:34 +0900

    .

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,17 +7,17 @@ public class Test {
     System.out.println("===> " + value);
   }
 
-  static void print(String str) {
-    System.out.println("===> " + str);
-    System.out.println("===> " + str.toString());
-    // 둘이 같은 코드
+  static void print(float f) {
+    System.out.println("===> " + f);
   }
 
-  static void print(Date date) {
-    System.out.println("===> " + date);
+  static void print(Object obj) {
+    System.out.println("===> " + obj.toString());
   }
+
   public static void main(String[] args) {
     print(100);
+    print(3.14f);
     print("홍길동");
     print(new Date(System.currentTimeMillis()));
   }
