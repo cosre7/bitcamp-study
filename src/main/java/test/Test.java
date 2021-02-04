73c590a3004a99d56ffce2f1ddad356186ec2dc1
cosre7 <cosre7@gmail.com>
2021-02-04 12:00:47 +0900

    .

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,16 +4,21 @@ import java.sql.Date;
 
 public class Test {
 
-  static void print(Object obj) {
-    System.out.println("===> " + obj.toString());
-    // string과 date -> 다형적 변수 이용
-  }
 
   public static void main(String[] args) {
-    print(Integer.valueOf(100));
-    print(Float.valueOf(3.14f));
-    print("홍길동");
-    print(new Date(System.currentTimeMillis()));
-  }
+    int[] arr1 = new int[5];
+    float[] arr2 = new float[5];
 
+    arr1[0] = 100;
+    // arr1[2] = 3.14f; -> 불가능
+
+    Object[] arr3 = new Object[5];
+
+    arr3[0] = new String("hello");
+    arr3[1] = new Date(System.currentTimeMillis());
+    arr3[2] = new StringBuffer("Hello");
+    arr3[3] = Integer.valueOf(100);
+    arr3[4] = Float.valueOf(3.14f);
+    // 프리머티브 타입을 객체로 다룰 수 있다 -> wrapper 클래스의 장점
+  }
 }
