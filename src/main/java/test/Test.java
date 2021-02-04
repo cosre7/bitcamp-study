95bed6cc5479ff0c24c0600be8a066725fefaa6e
cosre7 <cosre7@gmail.com>
2021-02-04 11:55:50 +0900

    .

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,13 +3,6 @@ package test;
 import java.sql.Date;
 
 public class Test {
-  static void print(int value) {
-    System.out.println("===> " + value);
-  }
-
-  static void print(float f) {
-    System.out.println("===> " + f);
-  }
 
   static void print(Object obj) {
     System.out.println("===> " + obj.toString());
@@ -17,8 +10,8 @@ public class Test {
   }
 
   public static void main(String[] args) {
-    print(100);
-    print(3.14f);
+    print(Integer.valueOf(100));
+    print(Float.valueOf(3.14f));
     print("홍길동");
     print(new Date(System.currentTimeMillis()));
   }
