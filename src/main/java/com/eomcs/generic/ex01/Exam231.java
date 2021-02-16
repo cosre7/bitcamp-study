f8ee1ba9d0586a56409ec380b196beb98355c3d0
cosre7 <cosre7@gmail.com>
2021-02-16 11:41:26 +0900

    .

--- src/main/java/com/eomcs/generic/ex01/Exam231.java
+++ src/main/java/com/eomcs/generic/ex01/Exam231.java
@@ -4,27 +4,25 @@ package com.eomcs.generic.ex01;
 import java.util.Date;
 import com.eomcs.generic.ex02.Member;
 
+// 2) 제네릭을 적용한 클래스
+// - 클래스 전체에서 사용할 "타입 파라미터(타입 이름을 저장하는 변수)"를 선언하기
+//      class 클래스명<타입파라미터명, 타입파라미터명, ...> {...}
+// - 클래스가 다루는 어떤 타입이 있다고 가정하자. 그 타입을 T라고 부르겠다.
+//
+class Box<T> {
+  T value;  // T라는 타입의 인스턴스 주소를 저장할 필드 
+  // 클래스를 쓰는 시점에 지정한 타입을 T라는 별명으로 받겠다
 
-
-public class Exam0230 {
-
-  //2) 제네릭을 적용한 클래스
-  //- 클래스 전체에서 사용할 "타입 파라미터(타입 이름을 저장하는 변수)"를 선언하기
-  //     class 클래스명<타입파라미터명, 타입파라미터명, ...> {...}
-  //- 클래스가 다루는 어떤 타입이 있다고 가정하자. 그 타입을 T라고 부르겠다.
-  //
-  static class Box<T> {
-    T value;  // T라는 타입의 인스턴스 주소를 저장할 필드 
-    // 클래스를 쓰는 시점에 지정한 타입을 T라는 별명으로 받겠다
-
-    public T get() { // T라는 타입의 객체를 리턴
-      return this.value;
-    }
-
-    public void set(T value) { // T라는 타입의 객체를 받을 파라미터
-      this.value = value;
-    }
+  public T get() { // T라는 타입의 객체를 리턴
+    return this.value;
   }
+
+  public void set(T value) { // T라는 타입의 객체를 받을 파라미터
+    this.value = value;
+  }
+}
+
+public class Exam231 {
 
   public static void main(String[] args) {
 
