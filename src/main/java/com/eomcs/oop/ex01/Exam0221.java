652a72900ec84cde67114e8805abf4dd9ddef8f3
cosre7 <cosre7@gmail.com>
2021-02-20 18:44:44 +0900

    .

--- src/main/java/com/eomcs/oop/ex01/Exam0221.java
+++ src/main/java/com/eomcs/oop/ex01/Exam0221.java
@@ -15,28 +15,28 @@ public class Exam0221 {
       float aver;
     }
 
-    Score[] arr = new Score[3];
+    Score[] arr = new Score[3]; // 3개짜리 배열 만들기 -> 0번~2번의 자리가 생긴다.
 
     arr[0] = new Score();
     arr[1] = new Score();
     arr[2] = new Score();
 
     // 배열의 각 항목에 저장된 인스턴스는 '배열명[인덱스]'를 사용하여 지정할 수 있다.
-    arr[0].name = "홍길동";
+    arr[0].name = "홍길동"; //0번 자리에 값 저장
     arr[0].kor = 100;
     arr[0].eng = 100;
     arr[0].math = 100;
     arr[0].sum = arr[0].kor + arr[0].eng + arr[0].math;
     arr[0].aver = arr[0].sum / 3f;
 
-    arr[1].name = "임꺽정";
+    arr[1].name = "임꺽정"; //1번 자리에 값 저장
     arr[1].kor = 90;
     arr[1].eng = 90;
     arr[1].math = 90;
     arr[1].sum = arr[1].kor + arr[1].eng + arr[1].math;
     arr[1].aver = arr[1].sum / 3f;
 
-    arr[2].name = "유관순";
+    arr[2].name = "유관순"; //2번 자리에 값 저장
     arr[2].kor = 80;
     arr[2].eng = 80;
     arr[2].math = 80;
