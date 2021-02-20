b04754a68ec88d963bf6343d8064d69041ee060a
cosre7 <cosre7@gmail.com>
2021-02-20 18:40:09 +0900

    .

--- src/main/java/com/eomcs/oop/ex01/Exam0210.java
+++ src/main/java/com/eomcs/oop/ex01/Exam0210.java
@@ -50,6 +50,9 @@ public class Exam0210 {
     System.out.printf("%s: %d, %d, %d, %d, %.1f\n", s1.name, s1.kor, s1.eng, s1.math, s1.sum, s1.aver);
     System.out.printf("%s: %d, %d, %d, %d, %.1f\n", s2.name, s2.kor, s2.eng, s2.math, s2.sum, s2.aver);
     System.out.printf("%s: %d, %d, %d, %d, %.1f\n", s3.name, s3.kor, s3.eng, s3.math, s3.sum, s3.aver);
+
+    // 필요한 값의 양만큼 인스턴스 주소 필요 -> 각각에 맞춰 레퍼런스 준비 -> 보관할 값이 늘어날수록
+    // 인스턴스 주소, 레퍼런스도 늘어나게 된다.
   }
 }
 
