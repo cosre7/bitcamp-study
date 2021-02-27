b02dfc3c95c45a2f9efad6db4c26c6daa81e7edc
cosre7 <cosre7@gmail.com>
2021-02-27 23:41:06 +0900

    .

--- src/main/java/com/eomcs/lang/ex03/Exam0220.java
+++ src/main/java/com/eomcs/lang/ex03/Exam0220.java
@@ -10,6 +10,7 @@ public class Exam0220 {
     System.out.println(12783406);
     System.out.println(1278_3406); // 일상에서 사용하는 1278,3406 같은 느낌
     System.out.println(12_783_406); // 일상에서 사용하는 12,783,406 같은 느낌
+    // 출력값은 12783406 으로 동일하다.
 
     //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
     //System.out.println(_12783406); 
@@ -19,6 +20,7 @@ public class Exam0220 {
     System.out.println(077);
     System.out.println(0_77);
     System.out.println(07_7);
+    // 출력값은 63으로 동일하다.
 
     //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
     //System.out.println(_077);
@@ -28,6 +30,7 @@ public class Exam0220 {
     System.out.println(0b1100100);
     System.out.println(0b110_0100);
     System.out.println(0b1_1_0_0_1_0_0);
+    // 출력값은 100으로 동일하다.
 
     //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
     //System.out.println(0b_1100100);
@@ -36,6 +39,7 @@ public class Exam0220 {
     //## 16진수에 _ 문자를 삽입하기
     System.out.println(0xffaa);
     System.out.println(0xff_aa);
+    // 출력값은 65450으로 동일하다.
 
     //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
     //System.out.println(0x_ffaa);
