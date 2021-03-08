00d86ffc61916e9d0743ced751c36d7c86282214
cosre7 <cosre7@gmail.com>
2021-03-08 10:55:15 +0900

    .

--- src/main/java/com/eomcs/net/ex06/HttpClient.java
+++ src/main/java/com/eomcs/net/ex06/HttpClient.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 // HTTP 요청 프로토콜
 // ---------------------------------
 // GET [자원주소] HTTP/1.1 (CRLF)
-// Host: [서버주소] (CRLF)
+// Host: [서버주소] (CRLF) // 요청헤더
 // (CRLF)
 // ---------------------------------
+// => 최소 hppt 요청 프로토콜
 //
 // 프로토콜(protocol)?
 // => 클라이언트/서버 간의 통신 규칙.
@@ -24,11 +25,11 @@ public class HttpClient {
 
     // HTTP 요청 프로토콜에 따라 서버에 데이터 전송
     // => macOS에서 JVM을 실행할 때, println()은 문자열 뒤에 0a(LF) 코드만 붙인다.
-    // => 이를 해결하려면, 다음과 같이 명확하게 CRLF 코드를 붙여라.
+    // => 이를 해결하려면, 다음과 같이 명확하게 CRLF 코드(\r\n)를 붙여라.
     //
-    out.print("GET /DetailView.aspx?itemno=C204190906 HTTP/1.1\r\n");
-    out.print("Host: itempage3.auction.co.kr\r\n");
-    out.print("\r\n");
+    out.print("GET /DetailView.aspx?itemno=C204190906 HTTP/1.1\r\n"); // request-line
+    out.print("Host: itempage3.auction.co.kr\r\n"); // header
+    out.print("\r\n"); // 엔터
 
     // HTTP 응답 프로토콜에 따라 서버가 보낸 데이터를 수신
     while (true) {
--- src/main/java/com/eomcs/net/ex06/HttpServer.java
+++ src/main/java/com/eomcs/net/ex06/HttpServer.java
@@ -27,7 +27,7 @@ public class HttpServer {
       while (true) {
         String str = in.nextLine();
         System.out.println(str);
-        if (str.equals(""))
+        if (str.equals("")) // 빈 줄
           break;
       }
 
@@ -35,10 +35,10 @@ public class HttpServer {
       // => macOS에서 JVM을 실행할 때, println()은 문자열 뒤에 0a(LF) 코드만 붙인다.
       // => 이를 해결하려면, 다음과 같이 명확하게 CRLF 코드를 붙여라.
       //
-      out.print("HTTP/1.1 200 OK\r\n");
-      out.print("Content-Type: text/html; charset=UTF-8\r\n");
-      out.print("\r\n");
-      out.print("<html><body><h1>안녕!-강사</h1></body></html>\r\n");
+      out.print("HTTP/1.1 200 OK\r\n"); // status-line
+      out.print("Content-Type: text/html; charset=UTF-8\r\n"); // header
+      out.print("\r\n"); // 엔터
+      out.print("<html><body><h1>안녕!-박유진</h1></body></html>\r\n"); // message-body
 
       out.close();
       in.close();
