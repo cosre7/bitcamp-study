26abc938a8ebab08b9a28551460f61623330fe31
cosre7 <cosre7@gmail.com>
2021-03-24 10:50:03 +0900

    .

--- src/main/java/com/eomcs/jdbc/ex1/Exam0210.java
+++ src/main/java/com/eomcs/jdbc/ex1/Exam0210.java
@@ -22,34 +22,42 @@ public class Exam0210 {
       // username : DBMS 사용자 아이디
       // password : DBMS 사용자 암호
       //
-      con = DriverManager.getConnection( //
-          "jdbc:mariadb://localhost:3306/studydb", // jdbcURL
-          "study", // username
-          "1111" // password
-          );
-      // => DriverManager는 등록된 java.sql.Driver 구현체 중에서
-      // jdbc url에 지정된 Driver 객체를 찾는다.
-      // 예) MariaDB: org.mariadb.jdbc.Driver 클래스의 객체
-      // => DB 연결을 Driver 구현체에게 위임한다.
-      // 즉, Driver 객체의 connect()를 호출한다.
-      // => Driver 구현체(org.mariadb.jdbc.Driver 객체)는 DBMS와 연결한 후
-      // 소켓 정보를 갖고 있는 java.sql.Connection 구현체를 리턴한다.
-      //
-      // [App]...................[DriverManager].............[Driver 구현체]
-      // ..| getConnection()............|............................|
-      // ..|--------------------------->|............................|
-      // ..|............................|......connect().............|
-      // ..|............................|--------------------------->|
-      // ..|............................|............................|-> new MariaDbConnection()
-      // ..|............................|..........return............|
-      // ..|........return..............|<---------------------------|
-      // ..|<---------------------------|............................|
-      //
-      System.out.println("DBMS와 연결됨!");
+      /*MariaDbConnection 이라고 선언할 필요가 없다
+       * 어차피 mariadb 커넥션 객체를 리턴하기 때문*/ con = DriverManager.getConnection( // -> mariadb 커넥션 객체를 리턴한다.
+           "jdbc:mariadb://localhost:3306/studydb", // jdbcURL
+           "study", // username
+           "1111" // password
+           );
 
-      // MariaDB의 Driver 구현체가 리턴한 Connection 객체는
-      // 어떤 클래스일까?
-      System.out.println(con.getClass().getName());
+       // 1. localhost가 아닌 192.168.2.2 같은 없는 서버주소를 써보자
+       // 2. 3306이 아닌 3307같은 다른 포트번호를 써보자
+       // 3. studydb2같은 다른 데이터베이스를 써보자
+       // 4. study가 아닌 다른 username을 써보자
+       // 5. password를 다르게 써보자
+
+       // => DriverManager는 등록된 java.sql.Driver 구현체 중에서
+       // jdbc url에 지정된 Driver 객체를 찾는다.
+       // 예) MariaDB: org.mariadb.jdbc.Driver 클래스의 객체
+       // => DB 연결을 Driver 구현체에게 위임한다.
+       // 즉, Driver 객체의 connect()를 호출한다.
+       // => Driver 구현체(org.mariadb.jdbc.Driver 객체)는 DBMS와 연결한 후
+       // 소켓 정보를 갖고 있는 java.sql.Connection 구현체를 리턴한다.
+       //
+       // [App]...................[DriverManager].............[Driver 구현체]
+       // ..| getConnection()............|............................|
+       // ..|--------------------------->|............................|
+       // ..|............................|......connect().............|
+       // ..|............................|--------------------------->|
+       // ..|............................|............................|-> new MariaDbConnection()
+       // ..|............................|..........return............|
+       // ..|........return..............|<---------------------------|
+       // ..|<---------------------------|............................|
+       //
+       System.out.println("DBMS와 연결됨!");
+
+       // MariaDB의 Driver 구현체가 리턴한 Connection 객체는
+       // 어떤 클래스일까?
+       System.out.println(con.getClass().getName());
 
     } finally {
       // 자원해제
