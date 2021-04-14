3f9cf3880f27efd278bd61bc169d76bdcd5a46aa
cosre7 <cosre7@gmail.com>
2021-04-14 20:03:02 +0900

    .

---
+++ src/main/java/com/eomcs/reflect/ex06/c/MyInvocationHandler.java
@@ -0,0 +1,18 @@
+package com.eomcs.reflect.ex06.c;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class MyInvocationHandler implements InvocationHandler {
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    switch (method.getName()) {
+      case "m1":
+        return 100; // 오토박싱 -> Integer.valueOf(100)으로 바뀐다는 뜻
+        // 다음과 같이 auto-boxing => Integer.valueOf(100)
+      case "m2":
+        return "Hello!";
+    }
+    return null; // void의 경우
+  }
+}
