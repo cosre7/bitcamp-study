280881434ff43fe87f85b6de289823b4a98853f3
cosre7 <cosre7@gmail.com>
2021-04-19 12:54:01 +0900

    Executors 태스크 프레임워크
    1. 스레드풀 종료

--- src/main/java/com/eomcs/concurrent/ex7/Exam0410.java
+++ src/main/java/com/eomcs/concurrent/ex7/Exam0410.java
@@ -1,4 +1,5 @@
 // Executors 태스크 프레임워크 - 스레드풀 종료 : shutdown()
+// 휴가를 써도 퇴사시키지 않는다.
 package com.eomcs.concurrent.ex7;
 
 import java.util.concurrent.ExecutorService;
@@ -16,15 +17,17 @@ public class Exam0410 {
     @Override
     public void run() {
       try {
-        System.out.printf("%s 스레드 실행 중...\n",
-            Thread.currentThread().getName());
+        System.out.printf("[%d] %s 스레드 실행 중...\n",
+            this.millisec, Thread.currentThread().getName());
 
         Thread.sleep(millisec);
 
-        System.out.printf("%s 스레드 종료!\n",
-            Thread.currentThread().getName());
+        System.out.printf("[%d] %s 스레드 종료!\n",
+            this.millisec, Thread.currentThread().getName());
+
       } catch (Exception e) {
-        System.out.printf("%s 스레드 실행 중 오류 발생!\n", Thread.currentThread().getName());
+        System.out.printf("[%d] %s 스레드 실행 중 오류 발생!\n", 
+            this.millisec, Thread.currentThread().getName());
       }
     }
   }
@@ -34,18 +37,21 @@ public class Exam0410 {
     executorService.execute(new MyRunnable(6000));
     executorService.execute(new MyRunnable(2000));
     executorService.execute(new MyRunnable(4000));
-    executorService.execute(new MyRunnable(4000));
+
+    executorService.execute(new MyRunnable(4000)); // 4번째부터 대기열
     executorService.execute(new MyRunnable(4000));
     executorService.execute(new MyRunnable(4000));
 
-    // 더이상 작업 요청을 받지 말고
-    // 이전에 요청한 작업들이 완료되면
-    // 스레드를 종료하도록 예약한다.
+    // => 더이상 작업 요청을 받지 말고
+    //    이전에 요청한 작업(대기하고 있는 작업)들이 완료되면
+    //    스레드를 종료하도록 예약한다.
+    // => 작업 중인 스레드가 Not Runnable 상태가 아니라면 
+    //    작업이 끝날 때까지 기다린다. 
     executorService.shutdown();
 
     // 작업 요청을 거절한다.
     // => 예외 발생!
-    executorService.execute(new MyRunnable(4000));
+    executorService.execute(new MyRunnable(4000)); // shutdown 후에는 주문안받음!
 
     System.out.println("main() 종료!");
   }
