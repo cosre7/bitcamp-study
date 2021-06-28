7817ff1590460cffb863738d5f3e43260bd56809
cosre7 <cosre7@gmail.com>
2021-06-28 22:48:05 +0900

    ex03-a

---
+++ src/main/java/com/eomcs/spring/ioc/ex03/a/Exam01.java
@@ -0,0 +1,20 @@
+// 생성자 호출 확인
+package com.eomcs.spring.ioc.ex03.a;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam01 {
+
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(//
+        "com/eomcs/spring/ioc/ex03/a/application-context.xml");
+
+    // SpringUtils.printBeanNames(iocContainer); 
+    // 클래스이름.<init> => 생성자 라는 뜻
+    // Engine에 기본생성자가 없기 때문에 에러!
+  }
+
+}
+
+
